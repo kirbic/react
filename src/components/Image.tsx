fc6d167c5e4d7775b52cbf3f52d9fdcd9e436ea6
Marc Pomar <marc@corecode.school>
2021-04-10 20:02:05 +0200

    fix: add density

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -5,14 +5,16 @@ import PropTypes from "prop-types";
 type Transforms = {
   width?: number;
   height?: number;
+  density?: number;
 };
 
-type ImageProps = Transforms & {
+type ImageProps = Omit<Transforms, "density"> & {
   media_id: string;
 };
 const shorteners: Record<string, string> = {
   width: "w",
   height: "h",
+  density: "d",
 };
 
 const chain_transforms = (t: Transforms) => {
@@ -27,13 +29,18 @@ export const Image: FC<ImageProps & ImgHTMLAttributes<any>> = ({
   media_id,
   ...props
 }) => {
-  const url = useMemo(() => {
+  const { src, srcset } = useMemo(() => {
     const base = "https://cdn.kirbic.com";
-    const transforms = chain_transforms(R.pick(Object.keys(shorteners), props));
-    return `${base}/@${transforms}/${media_id}`;
+    const transformers = R.pick(Object.keys(shorteners), props);
+    const transforms = chain_transforms(transformers);
+    const transforms_2x = chain_transforms({ ...transformers, density: 2 });
+    return {
+      src: `${base}/@${transforms}/${media_id}`,
+      srcset: `${base}/@${transforms}/${media_id} 1x,${base}/@${transforms_2x}/${media_id} 2x`,
+    };
   }, R.props(Object.keys(shorteners), props));
 
-  return <img src={url} {...props} />;
+  return <img src={src} srcSet={srcset} {...props} />;
 };
 Image.propTypes = {
   media_id: PropTypes.string.isRequired,
